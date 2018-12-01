e3278c2c9cee841b5ec260aeeea9c439fa18b928
Miguel Angel <ing.mosorio19@gmail.com>
2018-12-01 16:30:58 -0600

    [UPDATE] implement metods and update the class

--- js/models/Player.js
+++ js/models/Player.js
@@ -1,7 +1,17 @@
-class Player {
-  this.height = 0;
-  this.width = 0;
-  this.image
-  constructor() {
+class Player extends GameObject {
+  init() {
+    this.width = 400;
+    this.height = 600;
+
+    this.img.src = './assets/marco/marco.jpg';
+    this.img.onload = function(){
+      this.draw();
+    }.bind(this);
+  }
+
+  draw(){
+    this.context.drawImage(this.img, this.x + 30,this.y,35,40,20,this.canvas.height - 140,50,70);
+    //this.context.drawImage(this.img, this.x,this.y,35,40,200,this.canvas.height - 140,50,70);
+    //this.context.drawImage(this.img, this.x + this.width, this.y, this.width, this.height)
   }
 }
